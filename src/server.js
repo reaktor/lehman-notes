8b56a7ffe7e454d871e251a8c0990abdef5603d9
Ivan Manolov <ivan.manolov@reaktor.com>
2018-04-06 20:09:58 -0400

    Hello world

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout'}))
+app.set('views', './views')
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => res.render('home'))
+
+app.listen('3000', () => console.log('App is running on port 3000'))
