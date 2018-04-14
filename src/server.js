e97c861a99588b5d39aae583cc95bb4f6424ef04
Ian Fosbery <ian.fosbery@reaktor.com>
2018-04-13 23:16:44 -0400

    Add flash notifications

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+const session = require('cookie-session')
+const cookie = require('cookie-parser')
+const flash = require('connect-flash')
 const hbs = require('express-handlebars')
 const methOverride = require('method-override')
 
@@ -11,6 +14,9 @@ const app = express()
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout' }))
 app.set('views', './views')
 app.set('view engine', 'hbs')
+app.use(cookie('secret'))
+app.use(session({ name: 'sess', keys: ['kwy1', 'kdjf4'], maxAge: 60000 }))
+app.use(flash())
 app.use(methOverride('_method'))
 app.use(express.static('public'))
 
@@ -20,7 +26,8 @@ app.get('/', function(req, res) {
 
 app.get('/notes', async function(req, res) {
   const allNotes = await notes.all()
-  res.render('notes/index', { notes: allNotes })
+  const messages = req.flash()
+  res.render('notes/index', { notes: allNotes, messages: messages })
 })
 
 app.get('/notes/new', function(req, res) {
@@ -32,6 +39,7 @@ app.post('/notes/new', async function(req, res) {})
 app.delete('/notes/:id', async function(req, res) {
   const { id } = req.params
   await notes.delete(id)
+  req.flash('info', `Note with id: ${id} was deleted successfully.`)
   res.redirect('/notes')
 })
 
