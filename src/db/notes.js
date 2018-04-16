c242c0b8e9fa1c99c8c469a8c3bd34ca2aa5d9b4
Ivan Manolov <ivan.manolov@reaktor.com>
2018-04-16 12:58:32 -0400

    Save note

--- src/db/notes.js
+++ src/db/notes.js
@@ -5,11 +5,26 @@ async function getNotes() {
   return rows
 }
 
+async function createNote(title, body) {
+  const { rows } = await db.query(
+    'INSERT INTO notes(title, body) VALUES ($1, $2) RETURNING id',
+    [title, body]
+  )
+  return rows[0]
+}
+
+async function getNote(id) {
+  const { rows } = await db.query(`SELECT * FROM notes where id=$1`, [id])
+  return rows[0]
+}
+
 async function deleteNote(id) {
   await db.query('DELETE FROM notes WHERE notes.id = $1', [id])
 }
 
 module.exports = {
   all: getNotes,
+  new: createNote,
+  get: getNote,
   delete: deleteNote,
 }
--- src/server.js
+++ src/server.js
@@ -6,14 +6,18 @@ const cookie = require('cookie-parser')
 const flash = require('connect-flash')
 const hbs = require('express-handlebars')
 const methOverride = require('method-override')
+const bodyParser = require('body-parser')
 
 const notes = require('./db/notes')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'layout' }))
 app.set('views', './views')
 app.set('view engine', 'hbs')
+
 app.use(cookie('secret'))
 app.use(session({ name: 'sess', keys: ['kwy1', 'kdjf4'], maxAge: 60000 }))
 app.use(flash())
@@ -31,10 +35,23 @@ app.get('/notes', async function(req, res) {
 })
 
 app.get('/notes/new', function(req, res) {
-  res.render('notes/new', { body: '' })
+  res.render('notes/form', { title: '', body: '' })
 })
 
-app.post('/notes/new', async function(req, res) {})
+app.post('/notes/new', async function(req, res) {
+  const { title, body } = req.body
+  const { id } = await notes.new(title, body)
+  req.flash('info', `Note with id:${id} was created successfully.`)
+  res.redirect(`/notes/${id}`)
+})
+
+app.get('/notes/:id', async function(req, res) {
+  const { id } = req.params
+  const { title, body } = await notes.get(id)
+  console.log(title, body)
+  const messages = req.flash()
+  res.render(`notes/form`, { title, body, messages })
+})
 
 app.delete('/notes/:id', async function(req, res) {
   const { id } = req.params
